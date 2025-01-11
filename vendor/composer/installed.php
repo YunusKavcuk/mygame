bf6ddcf61381f9b1a490241bac58da96ec9083c8
YunusKavcuk <65766059+YunusKavcuk@users.noreply.github.com>
2025-01-12 00:42:09 +0300

    yunus
    1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '92350971013a2039f2ec647f0dc85b61d0280680',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '92350971013a2039f2ec647f0dc85b61d0280680',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
